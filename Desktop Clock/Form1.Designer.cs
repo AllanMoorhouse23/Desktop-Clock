689e1ea9dc35fcc2e623eaf4edda9440d1ec4503
AllanMoorhouse23 <allanmoorhouse.am@gmail.com>
2019-05-08 10:30:00 +1200

    Add project files.

---
+++ Desktop Clock/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Desktop_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 209);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Comfortaa", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(19, -16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(443, 138);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "00:00:00";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.DoubleClick += new System.EventHandler(this.label2_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Comfortaa", 47.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(-21, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(479, 209);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "00/00/0000";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(429, 209);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Clock";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Desktop Clock/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Desktop_Clock
+{
+    public partial class Form1 : Form
+    {
+        string windowed;
+        public Form1()
+        {
+            InitializeComponent();
+
+            string center = ConfigurationManager.AppSettings["Center"];
+            center = center.ToLower();
+
+            windowed = ConfigurationManager.AppSettings["Windowed"];
+            windowed = windowed.ToLower();
+
+            string onTop = ConfigurationManager.AppSettings["OnTop"];
+            onTop = onTop.ToLower();
+
+            string backgroundMode = ConfigurationManager.AppSettings["BackgroundMode"];
+            backgroundMode = backgroundMode.ToLower();
+
+            int fontRed = Convert.ToInt32(ConfigurationManager.AppSettings["FontRed"]);
+            int fontGreen = Convert.ToInt32(ConfigurationManager.AppSettings["FontGreen"]);
+            int fontBlue = Convert.ToInt32(ConfigurationManager.AppSettings["FontBlue"]);
+
+            if (center == "no")
+            {
+                string desktopX = ConfigurationManager.AppSettings["DesktopLocationX"];
+                string desktopY = ConfigurationManager.AppSettings["DesktopLocationY"];
+                this.SetDesktopLocation(Convert.ToInt32(desktopX), Convert.ToInt32(desktopY));
+               
+            }
+            else if (center == "yes")
+            {
+                this.StartPosition = FormStartPosition.CenterScreen;
+            }
+
+            if (backgroundMode == "default")
+            {
+                //No transparency (standard look)
+                label2.Parent = label1;
+                label2.BackColor = Color.Transparent;
+                label1.Parent = pictureBox1;
+                label1.BackColor = Color.Transparent;
+                setFontColor(fontRed, fontGreen, fontBlue);
+                updateBackground();
+            }
+            else if (backgroundMode == "transparent")
+            {
+                //Completely transparent
+
+                if (fontRed > 0)
+                {
+                    this.TransparencyKey = Color.FromArgb(255, fontRed - 1, 255, 255);
+                    label1.BackColor = Color.FromArgb(255, fontRed - 1, 255, 255);
+                    label2.BackColor = Color.FromArgb(255, fontRed - 1, 255, 255);
+                    pictureBox1.BackColor = Color.FromArgb(255, fontRed - 1, 255, 255);
+                    this.BackColor = Color.FromArgb(255, fontRed - 1, 255, 255);
+                }
+                else {
+                    this.TransparencyKey = Color.FromArgb(255, fontRed + 1, 255, 255);
+                    label1.BackColor = Color.FromArgb(255, fontRed + 1, 255, 255);
+                    label2.BackColor = Color.FromArgb(255, fontRed + 1, 255, 255);
+                    pictureBox1.BackColor = Color.FromArgb(255, fontRed + 1, 255, 255);
+                    this.BackColor = Color.FromArgb(255, fontRed + 1, 255, 255);
+                }
+                if (windowed == "yes") {
+                    this.FormBorderStyle = FormBorderStyle.Fixed3D;
+                }
+                if (onTop == "yes")
+                {
+                    this.TopMost = true;
+                }
+
+
+                setFontColor(fontRed, fontGreen, fontBlue);
+
+                label2.Location = new Point(label2.Location.X - 20, label2.Location.Y);
+            }
+            else if(backgroundMode == "solidcolor"){
+
+                //Solid Color
+                int red = Convert.ToInt32(ConfigurationManager.AppSettings["Red"]);
+                int green = Convert.ToInt32(ConfigurationManager.AppSettings["Green"]);
+                int blue = Convert.ToInt32(ConfigurationManager.AppSettings["Blue"]);
+
+                label2.Location = new Point(label2.Location.X - 20, label2.Location.Y);
+                this.TransparencyKey = Color.FromArgb(0, 0, 0, 0);
+                this.BackColor = Color.FromArgb(255, red, green, blue);
+                setFontColor(fontRed, fontGreen, fontBlue);
+
+                if (windowed == "yes")
+                {
+                    this.FormBorderStyle = FormBorderStyle.Fixed3D;
+                }
+                if (onTop == "yes")
+                {
+                    this.TopMost = true;
+                }
+            }
+
+            
+            
+            timer1.Start();
+            
+            
+        }
+
+        private void setFontColor(int red, int green, int blue) {
+            label1.ForeColor = Color.FromArgb(255, red, green, blue);
+            label2.ForeColor = Color.FromArgb(255, red, green, blue);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            string time = "";
+            //get current time
+            int hh = DateTime.Now.Hour;
+            int mm = DateTime.Now.Minute;
+            int ss = DateTime.Now.Second;
+
+            
+            //padding leading zero
+            if (hh < 10)
+            {
+                time += "0" + hh;
+            }
+            else
+            {
+                time += hh;
+            }
+            time += ":";
+
+            if (mm < 10)
+            {
+                time += "0" + mm;
+            }
+            else
+            {
+                time += mm;
+            }
+            time += ":";
+
+            if (ss < 10)
+            {
+                time += "0" + ss;
+            }
+            else
+            {
+                time += ss;
+            }
+
+         
+            
+            label2.Text = time;
+            label1.Text = DateTime.Now.ToShortDateString();
+        }
+
+       
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+            
+        }
+
+        private void label2_DoubleClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void updateBackground() {
+
+
+            string alpha = ConfigurationManager.AppSettings["Alpha"];
+            string red = ConfigurationManager.AppSettings["Red"];
+            string green = ConfigurationManager.AppSettings["Green"];
+            string blue = ConfigurationManager.AppSettings["Blue"];
+
+            this.WindowState = FormWindowState.Minimized;
+            Rectangle aRectangle = new Rectangle(0, 0, this.Width, this.Height);
+            SolidBrush br = new SolidBrush(Color.FromArgb(Convert.ToInt32(alpha), Convert.ToInt32(red), Convert.ToInt32(green), Convert.ToInt32(blue)));
+            //SolidBrush br = new SolidBrush(Color.FromArgb(105, 0, 0, 0));
+            Bitmap bitmap = new Bitmap(this.Width, this.Height);
+
+            Size s = this.Size;
+            Graphics g = Graphics.FromImage(bitmap as Image);
+            g.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, s);
+            g.FillRectangle(br, aRectangle);
+            this.WindowState = FormWindowState.Normal;
+           
+            pictureBox1.Image = bitmap;
+
+        }
+
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            if (windowed == "yes")
+            {
+                //Ignore the update event
+            }
+            else
+            {
+                updateBackground();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
